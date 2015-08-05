90e40386ed0d444c9079c4af0877143ab1f5a23f
Matt Fiocca <matt.fiocca@gmail.com>
2015-08-05 11:24:28 -0500

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# optimize-pantheon-images
+# Optimize Pantheon Images
 Simple bash script to optimize images on pantheon systems
+
+## Dependancies
+
+### jpegtran
+
+The existence of jpegtran on your local system, which is a command found in the libjpeg library, 
+a library that comes bundled with ImageMagick. If you’ve installed imagemagick for some other reason, 
+you’ve already got jpegtran installed. If not, it’s just a matter of: 
+
+**Ubuntu/Debian**
+```
+sudo apt-get install gcc
+sudo apt-get install imagemagick
+```
+
+**CentOS/Fedora/RHEL**
+```
+yum install gcc php-devel php-pear
+yum install ImageMagick ImageMagick-devel
+```
+
+**Mac** (using homebrew)
+```
+brew install imagemagick
+```
+
+### sshpass
+
+This script will prompt you once for your pantheon password, then sshpass will send your password on your behalf to both rsync calls.
+This is used so that you aren't prompted mutiple times for your pantheon password, so the script can just run. 
+
+```
+curl -O -L http://downloads.sourceforge.net/project/sshpass/sshpass/1.05/sshpass-1.05.tar.gz && tar xvzf sshpass-1.05.tar.gz
+./configure
+make
+sudo make install
+sshpass -V
+```
+
+If you do not wish to install sshpass, you can just omit the following from the beginning of the rsync calls:
+
+```
+sshpass -p "$PASSWORD"
+```
+
+### Bonus
+
+You’ll notice that I’ve wrapped this functionality into a bash function, and then just call the 
+function immediately after declaration. This is for fun things like sourcing into your ~/.bash_profile. 
+
+For example, remove the function call at the end of this script and save the script to `~/optimize_pantheon_jpgs.sh`. 
+Then add this to end of `~/.bash_profile`:
+
+```
+. ~/optimize_pantheon_jpgs.sh
+```
+
+Don’t forget the period at the beginning. Save and close, then start a new terminal session. Now, you can call optimize_pantheon_jpgs 
+directly on the terminal from anywhere and it will return you back to where you were terminal’ed into previous to the call.
---
+++ optimize_pantheon_images.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# This script relies on jpegtran, a tool included with libjpeg.
+# libjpeg comes bundled with ImageMagick on most nix platforms:
+# 
+# 	Mac: 
+# 	brew install imagemagick
+# 	
+# 	Ubuntu/Debian: 
+# 	sudo apt-get install gcc
+# 	sudo apt-get install imagemagick
+# 	
+# 	CentOS/Fedora/RHEL: 
+# 	yum install gcc php-devel php-pear
+# 	yum install ImageMagick ImageMagick-devel
+# 	
+# 	Windows:
+# 	.... well.... sorry, i'm not sure about this one. 
+
+
+function optimize_pantheon_images() {
+
+	# checks for the presence of jpegtran, which we use for image optimization
+	command -v jpegtran >/dev/null 2>&1 || { echo >&2 "I require jpegtran but it's not installed. Aborting."; exit 1; }
+
+	# checks to make sure the desired working directory exists
+	read -p "Local Files Root Directory (./files): " DIR
+	if [ ! -d $DIR ]; then
+		echo "$DIR is not a directory. Aborting"
+		exit 1
+	fi
+
+	# ask for the env
+	read -p "Pantheon Environment (dev|test|live): " PANTHEON_ENV
+	export ENV=$PANTHEON_ENV
+
+	# ask for the site id
+	read -p "Pantheon Site ID (XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX): " PANTHEON_UUID
+	export SITE=$PANTHEON_UUID
+
+	# ask for the pantheon password
+	read -sp "Pantheon Password: " PASSWORD
+
+	# remember where we started off so we can return later
+	# cd into working directoy
+	echo "Changing to $DIR"
+	CUR_DIR=$(pwd)
+	cd $DIR
+
+	# download files
+	echo "Downloading Files"
+	sshpass -p "$PASSWORD" rsync --partial -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' $ENV.$SITE@appserver.$ENV.$SITE.drush.in:files/* ./files/
+	if [ "$?" = "0" ] ; then
+		echo "Download completed"
+		exit
+	else
+		echo "Download failure. Backing off and retrying..."
+		sleep 180
+	fi
+
+	# optimize jpgs
+	FILES=$(find . -iname '*.jpg' -o -iname '*.jpeg')
+	for F in $FILES; do
+		echo "Optimizing: $F"
+		jpegtran -copy none -progressive -outfile $F $F
+	done
+
+	# upload files
+	echo "Uploading Files"
+	sshpass -p "$PASSWORD" rsync --partial -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' . --temp-dir=../tmp/ $ENV.$SITE@appserver.$ENV.$SITE.drush.in:files/.
+	if [ "$?" = "0" ] ; then
+		echo "Upload completed"
+		exit
+	else
+		echo "Upload failure. Backing off and retrying..."
+		sleep 180
+	fi
+
+	echo "Done"
+
+	# return back to where we came from
+	cd $CUR_DIR
+}
+
+optimize_pantheon_images
